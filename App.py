f55b95beef89c251f7f9bd18b1ec74409c65f16e
Adish Ansari <adishansari786@gmail.com>
2025-07-27 17:43:47 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
 s = "malayalam"  # string
+
 i,j = 0, len(s) - 1  # two pointers
+
 is_palindrome = True  # assume palindrome
+
 while i < j:
     if s[i] != s[j]:  # mismatch found
         is_palindrome = False
